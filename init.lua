b0f6334f625e808292f5ad38480a42789ac93a0c
ErvinMesa <jeremiahjmesa@gmail.com>
2024-12-20 14:43:21 +0800

    Added harpoon

--- init.lua
+++ init.lua
@@ -894,6 +894,7 @@ require('lazy').setup({
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.telescope',
   require 'kickstart.plugins.undotree',
+  require 'kickstart.plugins.harpoon',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -21,6 +21,11 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- Useful for getting pretty icons, but requires a Nerd Font.
     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    {
+      'ThePrimeagen/harpoon',
+      branch = 'harpoon2',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+    },
   },
   config = function()
     -- Telescope is a fuzzy finder that comes with a lot of different things that
